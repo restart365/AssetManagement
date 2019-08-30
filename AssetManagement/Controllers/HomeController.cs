808b27d15144d9a0033056492663a3983d5300ad
Nicholas Li <nicholas@dtiguardian.com>
2019-08-30 13:30:36 -0600

    Adding author page

--- AssetManagement/Controllers/HomeController.cs
+++ AssetManagement/Controllers/HomeController.cs
@@ -13,17 +13,15 @@ namespace AssetManagement.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Author()
+        {
             return View();
         }
     }
